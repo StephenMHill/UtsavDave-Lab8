47fe04ab1781fd0a3d66d2c2790479ffeeae31a4
Utsav Dave <utsavdave1997@gmail.com>
2020-03-10 11:14:37 -0400

    added

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :article do
-    
+    sequence(:title) { |n| "title_#{n}" }
+    content { 'This is content' }
+    user
   end
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    sequence(:message) { |n| "comment_#{n}" }
+    article
+    user
   end
 end
